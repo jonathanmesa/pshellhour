4a5d4bcf28aed632a3b3bb52afba4a7bc6bab7e6
Jonathan Mesa <53017957+jonathanmesa@users.noreply.github.com>
2020-05-20 20:21:49 -0500

    Add files via upload
    Initial Copy

---
+++ ErrorActionPreference.ps1
@@ -0,0 +1,37 @@
+<# 
+Demo : #$ErrorActionPreference
+#>
+
+#Check Current  Preference
+$ErrorActionPreference
+#Step Generate an Error
+Get-WmiObject Win32_BIOS -computer notonline 
+#Change error preference
+$ErrorActionPreference = "Inquire"
+$ErrorActionPreference = "Continue"
+$ErrorActionPreference = "SilentlyContinue"
+$ErrorActionPreference = "Stop"
+
+#Change Preference at the commandlet level
+Get-WmiObject Win32_BIOS -computer notonline  -ErrorAction Inquire 
+
+
+# Try and Catch 
+function Get-Bios {
+    BEGIN {
+        Remove-Item c:\errors.txt -ErrorAction SilentlyContinue
+    }
+    PROCESS {
+        Try {
+            Get-WmiObject Win32_BIOS -comp notonline -ea Stop
+    } Catch {
+        $_ | Out-File c:\errors.txt -append
+     }
+    }
+}
+
+# Build in Varianle for Errors
+$Error
+
+
+
